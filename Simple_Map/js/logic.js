70100905bd9b267dc54cded4214a44803d1a76d3
evflores001 <67989268+evflores001@users.noreply.github.com>
2021-01-23 12:42:11 -0800

    Adding earthquakes data

--- Simple_Map/js/logic.js
+++ Simple_Map/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([37.5, -122.5], 10);
+let map = L.map('mapid').setView([30, 30], 2);
 
 // Add GeoJSON data.
 let sanFranAirport =
@@ -66,6 +66,15 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-str
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
+});
+
+// Accessing the airport GeoJSON URL
+let airportData = 'https://github.com/evflores001/Mapping_Earthquakes/blob/main/majorAirports.json';
+
+d3.json(airportData).then(function(data) {
+    console.log(data);
+    // Creating a GeoJSON layer with the retrieved data.
+    L.geoJson(data).addTo(map);
 });
 
 // Then we add our 'graymap' tile layer to the map.
