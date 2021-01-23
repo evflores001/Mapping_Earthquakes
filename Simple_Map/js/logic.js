bbc12cc2fe7641330422ec4e5f18609fa4d35229
evflores001 <67989268+evflores001@users.noreply.github.com>
2021-01-23 13:35:48 -0800

    Adding GeoJSON lines

--- Simple_Map/js/logic.js
+++ Simple_Map/js/logic.js
@@ -92,12 +92,25 @@ let airportData = "https://raw.githubusercontent.com/evflores001/Mapping_Earthqu
 
 // Accessing the Toronto airline routes GeoJSON URL.
 let torontoData = "https://raw.githubusercontent.com/evflores001/Mapping_Earthquakes/main/torontoRoutes.json";
-d3.json(airportData).then(function(data) {
+
+// Create a style for the lines.
+let myStyle = {
+    color: "#ffffa1",
+    weight: 2
+}
+
+d3.json(torontoData).then(function(data) {
     console.log(data);
     // Creating a GeoJSON layer with the retrieved data.
-    L.geoJson(data)
+    L.geoJson(data, {
+        style: myStyle,
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: "
+            + feature.properties.dst + "</h3>");
+        }
+    })
     .addTo(map); 
 });
 
 // Then we add our 'graymap' tile layer to the map.
-light.addTo(map);
+dark.addTo(map);
