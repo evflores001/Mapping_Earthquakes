f8fd80f241306e4e83caacaaf61620dd461e0629
evflores001 <67989268+evflores001@users.noreply.github.com>
2021-01-23 13:25:30 -0800

    Adding Toronto Routs

--- Simple_Map/js/logic.js
+++ Simple_Map/js/logic.js
@@ -59,7 +59,7 @@ cityData.forEach(function(city) {
 
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -74,13 +74,13 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-    Street: streets,
+    Light: light,
     Dark: dark
 };
 
 // Create the map object with a center and zoom level.
 let map = L.map("mapid", {
-    center: [40.7, -94.5],
+    center: [44., -80.0],
     zoom: 4
 });
 
@@ -98,4 +98,4 @@ d3.json(airportData).then(function(data) {
 });
 
 // Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+light.addTo(map);
